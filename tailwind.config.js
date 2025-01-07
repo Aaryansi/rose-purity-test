594f6a6298e6e4d5096ec10036130524336ea636
Aaryan Singh <singha9@rose-hulman.edu>
2025-01-06 19:22:05 -0500

    Initial commit: Added Rose-Hulman Purity Test

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.html"], // Adjust this to match your HTML file paths
+  theme: {
+    extend: {
+      colors: {
+        maroon: '#900020',
+        'maroon-dark': '#750018',
+      },
+    },
+  },
+  plugins: [],
+};
